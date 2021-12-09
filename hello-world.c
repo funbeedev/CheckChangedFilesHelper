b12916792cb536eb97f4f34df652b6401b040dbc
fum <66768334+funbeedev@users.noreply.github.com>
2021-12-09 01:10:32 +0000

    add sample source files

---
+++ hello-world.c
@@ -0,0 +1,20 @@
+/*
+-------------------------------------------------------------------------------------
+ Tutorial: print statement in C
+-------------------------------------------------------------------------------------
+*/
+
+// This is a pre-processor directive. It includes the standard input output library in our program
+#include <stdio.h>
+
+// The function 'main' is the first entry point for the program
+int main()
+{
+    // The print function is used to output to the console
+    printf("\nhello-world.c\n");
+}
+/*
+-------------------------------------------------------------------------------------
+ Challenge: Edit the code above to print "hello universe!" to console
+-------------------------------------------------------------------------------------
+*/
